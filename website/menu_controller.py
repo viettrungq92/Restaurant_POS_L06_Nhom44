532477f5bd73bed1116db061991f058b80337742
VinceK19 <dtvince0310@gmail.com>
2021-11-05 00:39:29 +0700

    edit menu

--- website/menu_controller.py
+++ website/menu_controller.py
@@ -1,5 +1,5 @@
 # from website import app
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, abort
 import json
 
 menu = Blueprint('menu',__name__)
@@ -9,4 +9,32 @@ def display_menu(catagory= "all"):
     with open("website/menu.json","r") as file:
         menu = json.load(file)
     
-    return render_template("menu.html", menu = menu, catagory = catagory)
+    if catagory == "all":
+        all_menu = []
+        for dishs in menu.values():
+            all_menu.extend(dishs)
+        return render_template("menu.html", menu = all_menu)
+    elif catagory in menu.keys():
+        return render_template("menu.html", menu = menu[catagory])
+    else:
+        abort(404)
+
+# @menu.route("/menu")
+# def display_menu():
+#     with open("website/menu.json","r") as file:
+#         menu = json.load(file)
+    
+#     selected = []
+#     try:
+#         catagory = request.args["catagory"]
+#     except:
+#         for dishs in menu.values():
+#             selected.extend(dishs)
+#         return render_template("menu.html", menu = selected)
+
+#     if catagory in menu.keys():
+#         selected.extend(menu[catagory])
+#         return render_template("menu.html", menu = selected)
+#     else:
+#         abort(404)
+    
