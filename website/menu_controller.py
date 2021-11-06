218cb994856b872154993a9cd4faceaa104db8f3
Thinh Phuoc Nguyen <88044830+ThinhNguyen0311@users.noreply.github.com>
2021-11-06 17:49:35 +0700

    details - lan 5

--- website/menu_controller.py
+++ website/menu_controller.py
@@ -6,7 +6,7 @@ menu = Blueprint('menu',__name__)
 
 @menu.route("/menu/<catagory>")
 def display_menu(catagory= "all"):
-    with open("website/menu.json","r") as file:
+    with open("Restaurant_POS_L06_Nhom44/website/menu.json","r") as file:
         menu = json.load(file)
     
     if catagory == "all":
@@ -23,6 +23,6 @@ def display_menu(catagory= "all"):
 def details():
     (id, type) = request.args.values()
     print(id, type)
-    with open("/website/menu.json", "r") as f:
+    with open("Restaurant_POS_L06_Nhom44/website/menu.json", "r") as f:
         menu = json.load(f)
     return render_template("details.html",id = id,type = type,menu = menu)
