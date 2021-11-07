ac3836044d07f1e2f7dd15018c48445ebc454a83
Phuong Nguyen <phuong.nguyen0911@hcmut.edu.vn>
2021-11-07 16:33:03 +0700

    Add validate fields

---
+++ website/static/js/index.js
@@ -0,0 +1,61 @@
+$( document ).ready(function() {
+   $("#signupForm").validate({
+        rules: {
+            firstname: "required",
+            lastname:"required",
+
+            email: {
+                required: true,
+                email: true
+            },
+            numberphone: {
+                required: true,
+                digits: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            repassword: {
+                required: true,
+                minlength: 5,
+                equalTo: "#password"
+            },
+            address: "required",
+
+            user:"required",
+
+            loginpassword: "required",
+               
+
+        },
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            email:{
+                required: "Please enter a valid email address",
+                email: "email chua dung dinh dang"
+            },
+            numberphone: {
+                required: "Please enter your numberphone",
+                digits: "Numberphone chua dung dinh dang"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            repassword: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long",
+                equalTo: "Please enter the same password as above"
+            },
+            address: "Please enter your address",
+
+            user:"Please enter your email or phone number",
+            
+            loginpassword: "Please provide a password",
+        
+        },
+    });
+});
+
