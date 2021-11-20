ce2c9f1461375d9bfe89b551644f70d81334b2fe
Phuong Nguyen <phuong.nguyen0911@hcmut.edu.vn>
2021-11-20 19:23:00 +0700

    update login manager

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -24,7 +25,6 @@ def create_app():
 
     from .models import User
 
-    login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
